909e1a14715573ee8e1dcd32420492cce23af767
Romain DUBOIS <rdubois@hn-services.center>
2025-09-02 11:40:25 +0200

    minor correction

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const { Co2Component } = require('./functions.js');
 const { ExtendedCo2Component } = require('./extented.js');
 
 module.exports = {
-    Co2Component,
-    ExtendedCo2Component
+    Co2Component: Co2Component,
+    ExtendedCo2Component: ExtendedCo2Component
 };
